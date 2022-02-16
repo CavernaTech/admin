b65e262b8e941f3de916253d325392233ecbc037
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-02-15 23:28:22 -0300

    Criada estrutura base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+    return (
+        <Container>
+            <Typography variant="h3">Admin</Typography>
+        </Container>
+    );
+}
+
+export default App;
